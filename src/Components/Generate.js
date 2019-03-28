ac8fe24c6433f9163d705be092a45136b11dfbac
MattHelton <mjoelhelton@me.com>
2019-03-28 01:04:35 +0000

    Adds hidden id to idea and creates ideaElement to later remove hidden class from it, so that empty idea <p> is hidden until idea is generated

--- src/Components/Generate.js
+++ src/Components/Generate.js
@@ -9,7 +9,7 @@ class Generate extends React.Component {
 
         this.state = {
             choices: ['“Beginners guide to...”', 'Buying/gift guide', 'Checklist', 'Comparison piece', 'Create a calendar of upcoming events', 'FAQ', 'Flow chart', '“How to...”', 'Interview industry experts (inside or outside your company)', 'Links roundup', 'List of things to do/never do', 'Mythbusting article', 'Predict future trends', 'Quote piece', 'The biggest influences/influencers', 'Timeline', 'Top 10 list', 'Update old content', 'Write a review/recommend a product, service, etc', '“x things to learn from...” (this could be a conference, event, TV show, movie, anything)'],
-            chosenIdea: ''
+            chosenIdea: null
 
         }
     }
@@ -19,14 +19,14 @@ class Generate extends React.Component {
     chooseIdea = () => {
         const chosenIdea = this.state.choices[this.randomizer()]
         this.setState({ chosenIdea })
-
+        let ideaElement = document.getElementById('idea')
         
     }
     render() {
             return (
                 <div>
                     <Button type='button' className='btn-lrg' id='gen-btn' onClick={this.chooseIdea}>Generate</Button>
-                    <p id='idea'>{this.state.chosenIdea}</p>
+                    <p id='idea hidden'>{this.state.chosenIdea}</p>
                 </div>
             )
     }
